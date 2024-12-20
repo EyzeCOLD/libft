76abd22b60e6a1a7adfd26ec042fa990a25bb2e9
Jukka Aho <juaho@student.hive.fi>
2024-12-20 14:43:30 +0200

    added ft_atoi2: atoi with overflow detection

--- Makefile
+++ Makefile
@@ -19,7 +19,8 @@ SRCS = \
 	ft_toupper.c 	ft_lstclear.c	ft_lstadd_front.c \
 	ft_lstdelone.c	ft_lstiter.c	ft_lstadd_back.c \
 	ft_lstlast.c	ft_lstmap.c		ft_lstnew.c	\
-	ft_lstsize.c	ft_printf.c		ft_printf_utils.c
+	ft_lstsize.c	ft_printf.c		ft_printf_utils.c \
+	ft_atoi2.c
 
 HEADERS = \
 	libft.h	ft_printf.h
---
+++ ft_atoi2.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juaho <juaho@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 15:38:11 by juaho             #+#    #+#             */
+/*   Updated: 2024/11/14 12:14:02 by juaho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
+
+static	int	atoi2_inner(const char *a, int *i, int neg)
+{
+	int num;
+	int	overflow;
+
+	num = 0;
+	overflow = 0;
+	while (*a >= '0' && *a <= '9')
+	{
+		num *= 10;
+		if (neg)
+		{
+			num -= *a - '0';
+			if (!overflow && num > 0)
+				overflow = 1;
+		}
+		else
+		{
+			num += *a - '0';
+			if (!overflow && num < 0)
+				overflow = 1;
+		}
+		a++;
+	}
+	*i = num;
+	return (overflow);
+}
+
+int	ft_atoi2(const char *a, int *i)
+{
+	int		neg;
+
+	neg = 0;
+	while ((*a >= 9 && *a <= 13) || *a == ' ')
+		a++;
+	if (*a == '-' || *a == '+')
+	{
+		if (*a == '-')
+			neg = 1;
+		a++;
+	}
+	return (atoi2_inner(a, i, neg));
+}

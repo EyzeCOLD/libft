9e84bfeae6ff55fa36bd9ba10363c7983751539b
Jukka Aho <juaho@student.hive.fi>
2025-01-10 13:09:52 +0200

    ft_strjoinm.c added

--- libft.h
+++ libft.h
@@ -48,6 +48,7 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoinm(size_t	n, ...);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
@@ -75,5 +76,6 @@ int		ft_min(int n1, int n2);
 int		ft_max(int n1, int n2);
 int		ft_pow(int n, unsigned int p);
 char	*get_next_line(int fd);
+char	*ft_strmjoin(size_t n, ...);
 
 #endif
